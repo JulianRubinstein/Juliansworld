9e251da094e45c583fb473602fb2bf0e87b70868
Julian Rubinstein <>
2020-12-04 04:24:23 -0500

    added CORS

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 import requests
 import json
 
@@ -10,14 +11,17 @@ def get_sudoku(level=1):
     return sudoku
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 class Sudoku(Resource):
-   def get(self):
-       sudoku = get_sudoku()
-       return sudoku
+    def get(self):
+        sudoku = get_sudoku()
+        return sudoku
 
 api.add_resource(Sudoku, "/")
 
 if __name__=="__main__":
     app.run(port=5003, debug=True)
+
+#http://127.0.0.1:5003/
